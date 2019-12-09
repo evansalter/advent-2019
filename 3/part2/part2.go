025af28a044106438701e737224018d2d41eb283
esalter-va <evanearlsalter@gmail.com>
2019-12-09 10:55:10 -0600

    Day 3 Part 2

--- 3/part2/part2.go
+++ 3/part2/part2.go
@@ -1,7 +1,136 @@
 package part2
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+	"strconv"
+	"strings"
+
+	"github.com/evansalter/advent-2019/helpers"
+)
+
+const (
+	wire         = "w"
+	intersection = "X"
+)
+
+type point struct {
+	X int
+	Y int
+}
+
+func (p point) Manhattan() int {
+	x, y := p.X, p.Y
+	if x < 0 {
+		x = -x
+	}
+	if y < 0 {
+		y = -y
+	}
+	return x + y
+}
+
+func (p point) Steps(targetPoint point, wirePoints [][]point) int {
+	count := 0
+	for _, w := range wirePoints {
+		for _, p := range w {
+			count++
+			if p == targetPoint {
+				break
+			}
+		}
+	}
+	return count
+}
+
+func (p point) ToPointDistance() pointDistance {
+	return pointDistance{
+		P:    p,
+		Dist: p.Manhattan(),
+	}
+}
+
+func (p point) ToPointSteps(targetPoint point, wirePoints [][]point) pointDistance {
+	return pointDistance{
+		P:    p,
+		Dist: p.Steps(targetPoint, wirePoints),
+	}
+}
+
+type pointDistance struct {
+	P    point
+	Dist int
+}
+
+func computePoint(curX, curY int, dir string) (x, y int) {
+	x, y = curX, curY
+	switch dir {
+	case "U":
+		y++
+	case "R":
+		x++
+	case "D":
+		y--
+	case "L":
+		x--
+	}
+	return x, y
+}
+
+func allIntsInSliceNonZero(slice []int) bool {
+	for _, x := range slice {
+		if x <= 0 {
+			return false
+		}
+	}
+	return true
+}
 
 func Run() {
-	fmt.Println("Answer")
+	lines := helpers.ReadInputFile(3)
+
+	wirePoints := make([][]point, len(lines))
+
+	for i, l := range lines {
+		commands := strings.Split(l, ",")
+		wirePoints[i] = make([]point, 0)
+
+		x, y := 0, 0
+		for _, c := range commands {
+			dir := c[:1]
+			mag, err := strconv.Atoi(c[1:])
+			if err != nil {
+				fmt.Printf("Error converting %s to int: %s\n", c[1:], err.Error())
+				return
+			}
+
+			for m := mag; m > 0; m-- {
+				x, y = computePoint(x, y, dir)
+				wirePoints[i] = append(wirePoints[i], point{X: x, Y: y})
+			}
+		}
+	}
+
+	pointCountMap := make(map[point][]int)
+	for i, w := range wirePoints {
+		for _, p := range w {
+			if _, ok := pointCountMap[p]; !ok {
+				pointCountMap[p] = make([]int, len(wirePoints))
+			}
+			pointCountMap[p][i]++
+		}
+	}
+
+	pointDistances := make([]pointDistance, 0)
+	for p, c := range pointCountMap {
+		if allIntsInSliceNonZero(c) {
+			pointDistances = append(pointDistances, p.ToPointSteps(p, wirePoints))
+		}
+	}
+
+	sort.Slice(pointDistances, func(i, j int) bool {
+		return pointDistances[i].Dist < pointDistances[j].Dist
+	})
+
+	fmt.Println(pointDistances[0].Dist)
 }
