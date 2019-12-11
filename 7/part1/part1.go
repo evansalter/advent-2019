8e34db9c98f29ee8add212b7cf02c676a9647de2
esalter-va <evanearlsalter@gmail.com>
2019-12-10 19:02:05 -0600

    Remove comment

--- 7/part1/part1.go
+++ 7/part1/part1.go
@@ -35,7 +35,6 @@ func findMaxInt(s []int) int {
 func Run() {
 	lines := helpers.ReadInputFile(7)[0]
 	input := strings.Split(lines, ",")
-	// input := strings.Split("3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0", ",")
 
 	perms := make([][]int, 0)
 	for i := 0; i <= 4; i++ {
